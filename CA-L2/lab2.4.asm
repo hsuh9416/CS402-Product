23a8755a3ca92dc49bdb6af24a5c508c43ea4a76
Haeun Suh <hsuh7@hawk.iit.edu>
2023-09-17 23:07:41 -0500

    Complete the lab2.tasm

--- CA-L2/lab2.4.asm
+++ CA-L2/lab2.4.asm
@@ -10,11 +10,11 @@ ext2:   .word 0
         .text
 main:  
 # load initial word of vars
-        lw $t0, -0x7fd0($gp)            # $t0 <- Memory[var1]
-        lw $t1, -0x7ffc($gp)            # $t1 <- Memory[var2]
+        lw $t0, var1                    # $t0 <- Memory[var1]
+        lw $t1, var2                    # $t1 <- Memory[var2]
 # store words from vars to exts
-        sw $t1, -0x7ff8($gp)            # ext1 = Memory[$gp - 0x7ff8] <- Memory[var2] = $t1
-        sw $t0, -0x7ff4($gp)            # ext2 = Memory[$gp - 0x7ff4] <- Memory[var1] = $t0
+        sw $t1, ext1            # ext1 = Memory[$gp - 0x7ff8] <- Memory[var2] = $t1
+        sw $t0, ext2            # ext2 = Memory[$gp - 0x7ff4] <- Memory[var1] = $t0
 # Exit
         li $v0, 10                      # Set code 10(exit)
         syscall                         # Syscall to exit
