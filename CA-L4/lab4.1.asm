b8edc48c72cdecc91f4b2e8b21710d0708c7efe1
Haeun Suh <hsuh7@hawk.iit.edu>
2023-09-26 12:24:49 -0500

    Fixed wrong file uploaded

--- CA-L4/lab4.1.asm
+++ CA-L4/lab4.1.asm
@@ -1,20 +1,14 @@
-        .data
-        .globl var1
-var1:   .word 0x0008   
-        .globl var2           
-var2:   .word 0x0007
-        .globl var3
-var3:   .word -0x07E7
         .text
-main:   lw $t0, var1            # $t0 <- Memory[var1]
-        lw $t1, var2            # $t1 <- Memory[var2]
-        lw $t2, var3            # $t2 <- Memory[var3]
-        beq $t0, $t1, If        # if (var1 == var2) goto If:
-        beq $0, $0, Else        # else goto Else:
-If:     sw $t2, var1            # $t0 = Memory[var1] <- $t2 = Memory[var3]
-        sw $t2, var2            # $t1 = Memory[var2] <- $t2 = Memory[var3]
-Else:   sw $t1, var1            # $t0 = Memory[var1] <- $t1 = Memory[var2]
-        sw $t0, var2            # $t1 = Memory[var2] <- $t0 = Memory[var1]
-Exit:
-        li $v0, 10              # Set code 10(exit)
-        syscall                 # Syscall to exit
+        .globl main
+main:   move $s0, $ra           # must save $ra since I'll have a call
+        jal test                # call 'text' with no parameters
+        nop                     # execute this after 'test' returns
+        move $ra, $s0           # restore the return address in $ra
+        jr $ra                  # return from main
+
+# The procedure 'test' does not call any other procedure. Therefore $ra
+# does not need to be saved. Since 'test' uses no registers there is
+# no need to save any registers.
+
+test:   nop                     # this is the procedure named 'test'
+        jr $ra                  # return from this procedure
