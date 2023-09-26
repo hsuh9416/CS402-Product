526efecea905d4487eed224e83a080fb66676ce1
Haeun Suh <hsuh7@hawk.iit.edu>
2023-09-26 15:31:09 -0500

    Added lab4.2.asm

---
+++ CA-L4/lab4.2.asm
@@ -0,0 +1,17 @@
+        .text
+        .globl main
+main:   add $sp, $sp, -4        # first update the stack pointer
+        # sub $sp, $sp, 4         # first update the stack pointer
+        sw $ra, 4($sp)          # push $ra into the stack
+        jal test                # call 'text' with no parameters
+        nop                     # execute this after 'test' returns
+        lw $ra, 4($sp)          # rist recover the data from the stack
+        add $sp, $sp, 4         # shrink the stack by one word
+        jr $ra                  # return from main
+
+# The procedure 'test' does not call any other procedure. Therefore $ra
+# does not need to be saved. Since 'test' uses no registers there is
+# no need to save any registers.
+
+test:   nop                     # this is the procedure named 'test'
+        jr $ra                  # return from this procedure
