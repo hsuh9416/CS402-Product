28d3c569c2e3ab784405aafdbc538c1176f68257
Haeun Suh <hsuh7@hawk.iit.edu>
2024-01-31 16:57:28 -0600

    Fix errors

--- CA-L2/Program/lab2.4.asm
+++ CA-L2/Program/lab2.4.asm
@@ -1,8 +1,9 @@
-        .data                           # Start from 0x10010000
+        .data                           # Start from default(0x10010000)
         .globl var1                     
 var1:   .word 0x02                      # Inital value: 2
         .globl var2                     
 var2:   .word 0x05                      # Inital value: 5
+        .data 0x10000000                # Start from different address(0x10000000)
         .extern ext1 4                  # $gp diff: -0x7ff8
 ext1:   .word 0x08                      # Inital value: 8
         .extern ext2 4                  # $gp diff: -0x7ff4
@@ -13,13 +14,9 @@ main:   addu $s0, $ra, $0               # save return address
 # load the data
         lw $t0, var1                    # Load the data from var1 to $t0
         lw $t1, var2                    # Load the data from var2 to $t1
-# load address: To ensure the program find its address
-        la $t3, ext1                    # Load the address from .extern ext1 tp $t0
 # copy from vars to exts
-        ; sw $t1, ext1                  # This command won't work!
-        ; sw $t0, ext2                  # This command won't work!
-        sw $t1, 0($t3)                  # Copy the data from $t1(var2) to ext1(at basis of $t3) 
-        sw $t0, 4($t3)                  # Copy the data from $t0(var1) to ext2(at diff 4 from $t3) 
+        sw $t1, ext1                    # Load the data from $t1(var2) to ext1
+        sw $t0, ext2                    # Load the data from $t0(var1) to ext2
 # restore the return address in $ra and return from main
         addu $ra, $0, $s0       # load the return address
         jr $ra                  # return from main
