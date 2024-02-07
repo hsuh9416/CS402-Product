cd7ad9879e76cfd90777e2b562e74a4b5b246ec6
hsuh7 <hsuh7@hawk.iit.edu>
2024-02-07 11:27:31 -0600

    update comment and fix errors

--- CA-L3/Program/lab3.5.asm
+++ CA-L3/Program/lab3.5.asm
@@ -1,39 +1,42 @@
                 .data
-msg1:           .asciiz "Please enter the fisrt integer number: "
+msg1:           .asciiz "Please enter the first integer number: "
 msg2:           .asciiz "Please enter the second integer number: "
 msg_far:        .asciiz "I'm far away\n"
 msg_near:       .asciiz "I'm nearby\n" 
 
                 .text
-main:   li $v0, 4               # system call for print_string 
-        la $a0, msg1            # a0 = the massage loaded from msg1
-        syscall             
+main:   li $v0, 4               # print string
+        la $a0, msg1            # load msg1 to $a0 for print
+        syscall                 # execute
         
-        li $v0, 5               # system call for read_int => read user input
-        syscall                 
+        li $v0, 5               # read user input - int
+        syscall                 # execute
         
-        addu $t0, $v0, $0       # t0 = v0 = first_number
+        addu $t0, $v0, $0       # load first number to $t0
 
-        li $v0, 4               # system call for print_string 
-        la $a0, msg2            # a0 = the massage loaded from msg2
-        syscall             
+        li $v0, 4               # print string 
+        la $a0, msg2            # load msg2 to $a0 for print
+        syscall                 # execute
 
-        li $v0, 5               # system call for read_int => read user input
-        syscall  
+        li $v0, 5               # read user input - int
+        syscall                 # execute
 
-        addu $t1, $v0, $0       # t1 = v0 = second_number
+        addu $t1, $v0, $0       # load second number to $t1
 
-        bne $t0, $t1, Near
-        # beq $0, $0, Far       # This command occurs error with wrong address.
-        j Far                   # To reach out to Far properly, Unconditionally jumps to the Far label
+        bne $t0, $t1, Near      # if ($t0 != $t1) goto Near 
+        # beq $0, $0, Far       # command occurs error with wrong address
+        j Far                   # unconditionally jumps to the Far label
         
-Near:   li $v0, 4               # system call for print_string 
-        la $a0, msg_near        # a0 = the massage loaded from msg_near
-        syscall     
-        j Exit
-        .space 32768            # Add 32 KB of space to make 'Far' label far away
-Far:    li $v0, 4               # system call for print_string 
-        la $a0, msg_far         # a0 = the massage loaded from msg_far
+Near:   li $v0, 4               # print string 
+        la $a0, msg_near        # load msg_near to $a0 for print
+        syscall                 # execute     
+        
+        j Exit                  # uncontitionally jumps to the exit label
+
+        .space 32768            # Add 32 KiB of space to make 'Far' label as far away
+
+Far:    li $v0, 4               # print sprint
+        la $a0, msg_far         # load msg_far to $a0 for print
         syscall     
 
 Exit:   li $v0, 10              # Set code 10(exit)
