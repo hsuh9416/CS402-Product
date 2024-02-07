899deaa8c7f1d56f0dc8150df4de2d987a274a3c
hsuh7 <hsuh7@hawk.iit.edu>
2024-02-07 10:49:12 -0600

    Update and fix error

--- CA-L3/Program/lab3.4.asm
+++ CA-L3/Program/lab3.4.asm
@@ -1,17 +1,17 @@
                 .data
                 .globl my_array
-my_array:       .space 10
-initial_value:  .word 0x08
+my_array:       .space 40       # total space: 4 X 10 = 40 
+initial_value:  .word 0x08      # initial value 8
                 .text
-main:   lw $t0, initial_value   # j = initial_value
+main:   lw $t0, initial_value   # j = initial_value = 8
         li $t1, 0               # i =  0
         li $t2, 10              # MAX = 10
         li $t3, 4               # multiply 4
 Loop:   bge $t1, $t2, Exit      # if (i >= 10) goto Exit
-        mul $t4, $t3, $t1       # diff = 4 * i
-        sw $t0, my_array($t4)   # my_array = j
-        addi $t1, $t1, 1        # i++;
-        addi $t0, $t0, 1        # j++;
-        j Loop
+        mul $t4, $t3, $t1       # diff from basis = 4 * i
+        sw $t0, my_array($t4)   # my_array[i] = j
+        addi $t1, $t1, 1        # i <- i + 1
+        addi $t0, $t0, 1        # j <- j + 1
+        j Loops
 Exit:   li $v0, 10              # Set code 10(exit)
         syscall                 # Syscall to exit
