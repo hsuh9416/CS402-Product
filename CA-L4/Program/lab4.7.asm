cfc449c74cb94eabfa9298235be7904afbd61ce6
Haeun Suh <hsuh7@hawk.iit.edu>
2024-02-24 11:18:18 -0600

    Fix errors

--- CA-L4/Program/lab4.7.asm
+++ CA-L4/Program/lab4.7.asm
@@ -11,8 +11,8 @@ cnt:    .word 0         # Counter for recursive calls
         .globl main
 main:   
         # Save return address
-        add $sp, $sp, -4
-        sw $ra, 4($sp)    
+        add $sp, $sp, -4      # Reserve the space for stack (current: -4)
+        sw $ra, 4($sp)        # Store the return address at -(4~0)
 
         # Set recursive call counter
         lw $t4, cnt            # init counter
@@ -32,7 +32,7 @@ main:
         syscall                 
         
         # Check the negativity
-        bltz $v0, err         # if ($v0 < 0) goto err  
+        bltz $v0, Error       # if ($v0 < 0) goto Error  
         addu $t0, $v0, $0     # else load $v0 to $t0
 
         # Promt the direction message for Second integer
@@ -45,7 +45,7 @@ main:
         syscall  
 
         # Check the negativity
-        bltz $v0, err         # if ($v0 < 0) goto Error
+        bltz $v0, Error       # if ($v0 < 0) goto Error
         addu $t1, $v0, $0     # else load $v0 to $t1
         
         # Start Ackermann
@@ -67,40 +67,43 @@ main:
         la $a0, ln            
         syscall
        
+        # Save the call counter
+        sw $t4, cnt            # cnt <- $t4
+
         # Restore address and stack point then exit
-        lw $ra, 4($sp)
-        add $sp, $sp, 4
+        lw $ra, 4($sp)          # Retrieve the return address    
+        add $sp, $sp, 4         # Restore the stack point (Resource release) (current: 0)
         jr $ra                  # Return from main
-err:                            # Warning & Loop again
+Error:                          # Warning & Loop again
         # Promt the error message
         li $v0, 4              
-        la $a0, msgD            
+        la $a0, mgsE            
         syscall
 
-        jal main                # Loop again
-Ackermann:                      # Breakpoint 0x004000cc  
+        jal main                # Restart loop
+Ackermann:                      
         # Initialize and save the address
-        add $sp, $sp, -12       # Reserve the space for stack
-        sw $ra, 12($sp)         # Store the return address at (12-8)    
-        sw $s0, 8($sp)          # Store the temporal first integer at (8-4)
-        sw $s1, 4($sp)          # Store the temporal second integer at (4-0)
+        add $sp, $sp, -12       # Reserve the space for stack (current: -16)
+        sw $ra, 12($sp)         # Store the return address at -(8-4)    
+        sw $s0, 8($sp)          # Store the temporal first integer at -(12-8)   
+        sw $s1, 4($sp)          # Store the temporal second integer at -(16-12)
 
         addi $t4, $t4, 1        # Increase the counter: cnt <- cnt + 1
 
         # Condition check for if x = 0 then y+1
-        bne $t0, $0, br1        # if( $t0 != 0) goto br1
+        bne $t0, $0, Branch1    # if( $t0 != 0) goto Branch1
         add $t3, $t1, 1         # if( $t0 == 0) $t3 <- $t1 + 1 (result <- y + 1)
 
         j Complete              # Goto Complete               
-br1:                            # For x != 0
+Branch1:                            # For x != 0
         # Condition check for else if y = 0 then A(x-1, 1)
-        bne $t1, $0, br2        # if( $t1 != 0) goto br2
+        bne $t1, $0, Branch2    # if( $t1 != 0) goto Branch2
         sub $t0, $t0, 1         # $t0 <- $t0 - 1 (x <- x-1)
         add $t1, $0, 1          # $t1 <- 0 + 1   (y <- 1)
         jal Ackermann           # Ackermann($t0, $t1) = A($t0 - 1, 1) (result <- A(x-1, 1))
 
         j Complete
-br2:                            # For y != 0
+Branch2:                        # For y != 0
         # Condition check for else A(x-1, A(x, y-1))
         sub $t1, $t1, 1         # $t1 <- $s1 - 1 (y <- y-1)
         move $s0, $t0           # $s0 <- $t0 (copy x to preserve the value)
@@ -109,11 +112,10 @@ br2:                            # For y != 0
         sub $t0, $s0, 1         # $t0 <- $s1 - 1 (x <- x -1) (use preserved x in $s0)
         move $t1, $t3           # $t1 <- A($t0, $t1 - 1) (y <- result = A(x, y - 1))
         jal Ackermann           # Ackermann($t0, $t1) = A($t0 - 1, A($t0, $t1 - 1)) (result <- A(x-1, A(x, y-1)))
-
 Complete:
         # Restore address, variables and stack point then return
         lw $ra, 12($sp)         # Retrieve the return address    
         lw $s0, 8($sp)          # Retrieve the temporal first integer
         lw $s1, 4($sp)          # Retrieve the temporal second integer
-        add $sp, $sp, 12        # Restore the stack point (Resource release)
-        jal $ra                 # return by the saved address
+        add $sp, $sp, 12        # Restore the stack point (Resource release) (current: -4)
+        jr $ra                 # return by the saved address
